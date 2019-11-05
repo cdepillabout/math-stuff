57f9bfd0eaceb5264b8c75538c8b4541a0d9625b
(cdep)illabout <cdep.illabout@gmail.com>
2019-11-06 06:37:20 +0900

    update

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,5 +1,8 @@
 module Lib where
 
+ffmap :: Functor f => Functor g => (a -> b) -> f (g a) -> f (g b)
+ffmap f = fmap (fmap f)
+
 numSamples :: Num a => [[a]] -> a
 numSamples = undefined
 
@@ -15,10 +18,14 @@ transpose = undefined
 calcMatrixMu :: [[a]] -> [[a]]
 calcMatrixMu = undefined
 
-matDiv :: [[a]] -> a -> [[a]]
-matDiv = undefined
+-- | Divide a matrix by a number.
+--
+-- >>> matDiv [ [10, 20, 30], [40, 50, 60], [70, 80, 90], [100, 110, 120] ] 10
+-- [[1.0,2,3],[4,5,6],[7,8,9],[10,11,12]]
+matDiv :: Fractional a => [[a]] -> a -> [[a]]
+matDiv mat = ffmap (/ a) mat
 
-calcCovarMatrix :: Num a => [[a]] -> [[a]]
+calcCovarMatrix :: Fractional a => [[a]] -> [[a]]
 calcCovarMatrix mat =
   let mu = calcMatrixMu mat
       mu' = transpose mu
