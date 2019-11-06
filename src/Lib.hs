1f4a757291f7f1322ea05454b94b27784d096c4d
(cdep)illabout <cdep.illabout@gmail.com>
2019-11-07 06:52:19 +0900

    Get covariance matrix calculation working.

--- src/Lib.hs
+++ src/Lib.hs
@@ -15,15 +15,96 @@ ffmap f = fmap (fmap f)
 numSamples :: Num a => [[a]] -> a
 numSamples = fromIntegral . length
 
-dotProd :: [[a]] -> [[a]] -> [[a]]
-dotProd = undefined
+-- | Calculate the dot product of two vectors.
+--
+-- >>> dotProdVec [1,2,3] [4,5,6]
+-- 32
+--
+-- >>> dotProdVec [] [1,2,3]
+-- *** Exception: dotProdVec: can't take a dot product when the first arg is an empty list.
+-- ...
+--
+-- >>> dotProdVec [1,2,3] []
+-- *** Exception: dotProdVec: can't take a dot product when the second arg is an empty list.
+-- ...
+--
+-- >>> dotProdVec [1,2,3] [4,5]
+-- *** Exception: dotProdVec: it doesn't make sense to take a dot product of vectors of two different lengths
+-- ...
+dotProdVec :: Num a => [a] -> [a] -> a
+dotProdVec [] _ = error "dotProdVec: can't take a dot product when the first arg is an empty list."
+dotProdVec _ [] = error "dotProdVec: can't take a dot product when the second arg is an empty list."
+dotProdVec v1 v2
+  | length v1 /= length v2 =
+  error "dotProdVec: it doesn't make sense to take a dot product of vectors of two different lengths"
+  | otherwise = sum $ zipWith (*) v1 v2
+
+-- | Calculate the number of rows in a  matrix.
+--
+-- >>> numRowsMat [[1,2],[3,4],[5,6]]
+-- 3
+--
+-- >>> numRowsMat [[]]
+-- 1
+--
+-- >>> numRowsMat []
+-- 0
+numRowsMat :: [[a]] -> Int
+numRowsMat = length
+
+-- | Calculate the number of columns in a matrix.
+--
+-- >>> numColsMat [[1,2],[3,4],[5,7]]
+-- 2
+--
+-- >>> numColsMat [[]]
+-- 0
+--
+-- >>> numColsMat []
+-- 0
+numColsMat :: [[a]] -> Int
+numColsMat = length . transpose
+
+-- | Calculate the dot product of two matricies.
+--
+-- >>> dotProdMat [[1,2],[3,4]] [[5,6],[7,8]]
+-- [[19,22],[43,50]]
+--
+-- >>> dotProdMat [[0,3,1]] [[5,2],[0,2],[10,1]]
+-- [[10,7]]
+--
+-- >>> dotProdMat [[3,9]] [[2],[-1]]
+-- [[-3]]
+--
+-- >>> dotProdMat [[4],[6],[-1]] [[5,2]]
+-- [[20,8],[30,12],[-5,-2]]
+--
+-- >>> dotProdMat [] []
+-- []
+--
+-- >>> dotProdMat [[1,2,3]] [[5,2],[0,2]]
+-- *** Exception: dotProdMat: number of columns of first matrix doesn't equal number of rows of second matrix
+-- ...
+dotProdMat :: forall a. Num a => [[a]] -> [[a]] -> [[a]]
+dotProdMat mat1 mat2
+  | numColsMat mat1 /= numRowsMat mat2 =
+    error "dotProdMat: number of columns of first matrix doesn't equal number of rows of second matrix"
+  | otherwise = foldl goRow [] mat1
+  where
+  goRow :: [[a]] -> [a] -> [[a]]
+  goRow accum rowOfMat1 =
+    let newRow = fmap goCol (transpose mat2)
+    in snoc accum newRow
+    where
+      goCol :: [a] -> a
+      goCol colOfMat2 = dotProdVec rowOfMat1 colOfMat2
 
 -- | Subtract one matrix from another.
 --
--- >>> matSubtract [[1,2,3],[4,5,6]] [[7,10,20],[1,3,10]]
+-- >>> subtractMat [[1,2,3],[4,5,6]] [[7,10,20],[1,3,10]]
 -- [[-6,-8,-17],[3,2,-4]]
-matSubtract :: forall a. Num a => [[a]] -> [[a]] -> [[a]]
-matSubtract mat1 mat2 =
+subtractMat :: forall a. Num a => [[a]] -> [[a]] -> [[a]]
+subtractMat mat1 mat2 =
   let xs :: [([a], [a])] = zip mat1 mat2
       ys :: [[(a,a)]] = fmap (uncurry zip) xs
   in ffmap (uncurry (-)) ys
@@ -74,8 +155,44 @@ appendAt [] (n, a) = error "appendAt: trying to append to the end of a row that 
 appendAt (h:t) (0, a) = snoc h a : t
 appendAt (h:t) (n, a) = h : appendAt t (n - 1, a)
 
-calcMatrixMu :: [[a]] -> [[a]]
-calcMatrixMu = undefined
+-- | Calculate the average of a list.
+--
+-- >>> calcAvg [10]
+-- 10.0
+--
+-- >>> calcAvg [1,5,9]
+-- 5.0
+--
+-- >>> calcAvg [100, 300, 700, 900]
+-- 500.0
+--
+-- >>> calcAvg []
+-- *** Exception: calcAvg: can't calculate the average of an empty list
+-- ...
+calcAvg :: Fractional a => [a] -> a
+calcAvg [] = error "calcAvg: can't calculate the average of an empty list"
+calcAvg lst = sum lst / fromIntegral (length lst)
+
+-- | Calculate the average for each column of a matrix.
+--
+-- >>> calcColAvgs [[1,2,3],[4,5,6],[7,8,9],[10,11,12]]
+-- [5.5,6.5,7.5]
+--
+-- >>> calcColAvgs []
+-- []
+calcColAvgs :: Fractional a => [[a]] -> [a]
+calcColAvgs mat =
+  let rows = transpose mat
+  in fmap calcAvg rows
+
+
+-- | Calculate the mus for a matrix (the average of each column), and then blow
+-- them back, repeating them for each row.
+--
+-- >>> calcMatrixMu [[1,2,3],[4,5,6],[7,8,9]]
+-- [[4.0,5.0,6.0],[4.0,5.0,6.0],[4.0,5.0,6.0]]
+calcMatrixMu :: Fractional a => [[a]] -> [[a]]
+calcMatrixMu mat = replicate (length mat) (calcColAvgs mat)
 
 -- | Divide a matrix by a number.
 --
@@ -84,13 +201,20 @@ calcMatrixMu = undefined
 matDiv :: Fractional a => [[a]] -> a -> [[a]]
 matDiv mat a = ffmap (/ a) mat
 
+-- | Calculate a covariance matrix.
+--
+-- >>> calcCovarMatrix [[1,2,3],[4,5,6],[7,8,9],[10,11,12]]
+-- [[11.25,11.25,11.25],[11.25,11.25,11.25],[11.25,11.25,11.25]]
+--
+-- >>> calcCovarMatrix [[90,60,90],[90,90,30],[60,60,60],[60,60,90],[30,30,30]]
+-- [[504.0,360.0,180.0],[360.0,360.0,0.0],[180.0,0.0,720.0]]
 calcCovarMatrix :: Fractional a => [[a]] -> [[a]]
 calcCovarMatrix mat =
   let mu = calcMatrixMu mat
       mu' = transpose mu
       mat' = transpose mat
-      dMat = matSubtract mat mu
-      dMat' = matSubtract mat' mu'
-      res = dotProd dMat' dMat
+      dMat = subtractMat mat mu
+      dMat' = subtractMat mat' mu'
+      res = dotProdMat dMat' dMat
       n = numSamples mat
   in matDiv res n
